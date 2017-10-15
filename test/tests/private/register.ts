1f773f95845649a83b39f38617882f147acafaef
mtsg <mtsg4code@outlook.jp>
2017-10-15 16:31:48 +0900

    Add test cases about register.

--- test/tests/private/register.ts
+++ test/tests/private/register.ts
@@ -4,17 +4,102 @@ import { RegisterTypes } from '../../../src/assets/ts/register';
 
 
 const INITIAL_CHARS = 'Hello World!';
-const INITIAL_CHARS_ARRAY: string[] = getCharArray(INITIAL_CHARS);
+const INITIAL_FIRST_WORD = 'Hello ';
 
 
 describe('register tests', function () {
+    describe('none types', () => {
+        it('none', async function () {
+            await new TestCase([INITIAL_CHARS])
+                .setRegister({ type: RegisterTypes.NONE, saved: null })
+                .sendKeys('12x')
+                .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                .done();
+        });
+    });
+
     describe('chars types', () => {
-        it('default', async function () {
-            let t = new TestCase([INITIAL_CHARS]);
-            t.setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] });
-            t.sendKeys('v$y');
-            t.expectRegister({ type: RegisterTypes.CHARS, saved: INITIAL_CHARS_ARRAY });
-            await t.done();
+        describe('cut', () => {
+            it('12x', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('12x')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                    .done();
+            });
+        });
+
+        describe('yank', () => {
+            it('v$y', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('v$y')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                    .done();
+            });
+            it('y$', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('y$')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                    .done();
+            });
+            it('yw', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('yw')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_FIRST_WORD) })
+                    .done();
+            });
+        });
+
+        describe('delete', () => {
+            it('v$d', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('v$d')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                    .done();
+            });
+            it('d$', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('d$')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_CHARS) })
+                    .done();
+            });
+            it('dw', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('dw')
+                    .expectRegister({ type: RegisterTypes.CHARS, saved: getCharArray(INITIAL_FIRST_WORD) })
+                    .done();
+            });
+        });
+    });
+
+    describe('serialized rows types', () => {
+        describe('yank', () => {
+            it('yy', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('yy')
+                    .expectRegister({ type: RegisterTypes.SERIALIZED_ROWS, saved: [getSerializedText(INITIAL_CHARS)] })
+                    .done();
+            });
+        });
+    });
+
+    describe('cloned rows types', () => {
+        describe('delete', () => {
+            it('dd', async function () {
+                await new TestCase([INITIAL_CHARS])
+                    .setRegister({ type: RegisterTypes.CHARS, saved: ['unchanged'] })
+                    .sendKeys('dd')
+                    .expectRegisterType(RegisterTypes.CLONED_ROWS)
+                    .expectRegister({ type: RegisterTypes.CLONED_ROWS, saved: [1] })
+                    .done();
+            });
         });
     });
 });
@@ -24,4 +109,8 @@ function getCharArray(str: string) {
     const result: string[] = [];
     [].forEach.call(str, (char: string) => result.push(char));
     return result;
+};
+
+function getSerializedText(text: string) {
+    return { text: text };
 };
